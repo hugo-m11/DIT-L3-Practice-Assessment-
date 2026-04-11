0962036823af36d4fdcba4d9a03af470164436ef
hugo-m11 <22hmawdsley@wakatipu.school.nz>
2026-04-11 13:06:06 +1200

    Add files via upload

--- DIT Practice assesment.py
+++ DIT Practice assesment.py
@@ -7,17 +7,17 @@ class SimpleGUI:
 
         self.input_frame.pack()
 
-        name_thing = StringVar()
-        age_thing = StringVar()
+        self.name_thing = StringVar()
+        self.age_thing = StringVar()
 
         self.name_label = Label(self.input_frame, text="Name")
         self.name_label.pack()
-        self.name_entry = Entry(self.input_frame, textvariable=name_thing)
+        self.name_entry = Entry(self.input_frame, textvariable=self.name_thing)
         self.name_entry.pack()
 
         self.age_label = Label(self.input_frame, text="Age")
         self.age_label.pack()
-        self.age_entry = Entry(self.input_frame, textvariable=age_thing)
+        self.age_entry = Entry(self.input_frame, textvariable=self.age_thing)
         self.age_entry.pack()
 
         self.phone_ownership = Label(self.input_frame, text="Do you have a phone?")
@@ -31,6 +31,19 @@ class SimpleGUI:
 
         self.own_phone_one = Radiobutton(self.input_frame, variable=self.v, text="No", value="2")
         self.own_phone_one.pack()
+
+        self.print_data_button = Button(self.input_frame, text="Print out data", command= self.print_data)
+        self.print_data_button.pack()
+    
+    def print_data(self):
+        name = self.name_thing.get()
+        age = self.age_thing.get()
+        phone = self.v.get()
+
+        print(name)
+        print(age)
+        print(phone)
+        
 
 
 if __name__ == "__main__":
