dede0501267786ea816637648387f77c1abafe90
hugo-m11 <22hmawdsley@wakatipu.school.nz>
2026-04-12 13:24:28 +1200

    Add files via upload

--- DIT Practice assesment.py
+++ DIT Practice assesment.py
@@ -9,12 +9,43 @@ class People:
 
 class SimpleGUI:
     def __init__(self, parent):
-        self.input_frame = Frame(parent)
-        self.display_frame = Frame(parent)
+        self.input_frame = Frame(parent, bg="pink")
+        self.display_frame = Frame(parent, bg="blue")
+
+        self.current_index = 0
 
         self.input_frame.pack()
 
         self.people = []
+
+        b1 = Button(self.input_frame, text="Click", command=self.switch_frame_two)
+        b1.pack()
+        b2 = Button(self.display_frame, text="Click", command=self.switch_frame_one)
+        b2.pack()
+
+        self.display_name = Label(self.display_frame, text="")
+        self.display_name.pack()
+
+        self.display_age = Label(self.display_frame, text="")
+        self.display_age.pack()
+
+        self.display_phone = Label(self.display_frame, text="")
+        self.display_phone.pack()
+
+        self.next_button = Button(self.display_frame, text="Next")
+        self.next_button.pack()
+
+        self.prev_button = Button(self.display_frame, text="Previous")
+        self.prev_button.pack()
+
+        self.back_button = Button(self.display_frame, text="Add New Person")
+        self.back_button.pack()
+
+
+
+
+
+
 
         self.name_thing = StringVar()
         self.age_thing = StringVar()
@@ -41,18 +72,37 @@ class SimpleGUI:
         self.own_phone_one = Radiobutton(self.input_frame, variable=self.v, text="No", value="2")
         self.own_phone_one.pack()
 
-        self.print_data_button = Button(self.input_frame, text="Print out data", command= self.print_data)
-        self.print_data_button.pack()
+        self.enter_data_button = Button(self.input_frame, text="enter data", command= self.enter_data)
+        self.enter_data_button.pack()
+
+        self.show_data_button = Button(self.input_frame, text="show data", command= self.show_all)
+        self.show_data_button.pack()
     
-    def print_data(self):
+    def switch_frame_one(self):
+        self.display_frame.pack_forget()
+        self.input_frame.pack()
+
+
+    def switch_frame_two(self):
+        self.input_frame.pack_forget()
+        self.display_frame.pack()
+
+
+    
+    def enter_data(self):
         name = self.name_thing.get()
         age = self.age_thing.get()
         phone = self.v.get()
-
         new_person = People(name, age, phone)
         self.people.append(new_person)
-        
-        print(len(self.people))
+
+    def show_all(self):
+        if len(self.people) == 0:
+            print("")
+            return
+
+        self.current_index = 0
+        self.switch_frame_two()
 
         
 
